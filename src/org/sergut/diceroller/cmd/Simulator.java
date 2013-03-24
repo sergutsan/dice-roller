b67a253cb0faddfe8380d43b75ca1af4e42006c9
Sergio Gutierrez-Santos "Sergut <sergut@gmail>
2013-03-24 20:42:38 +0100

    Added a command line program for the montecarlo simulator.

---
+++ src/org/sergut/diceroller/cmd/Simulator.java
@@ -0,0 +1,42 @@
+package org.sergut.diceroller.cmd;
+
+import org.sergut.diceroller.IllegalDiceExpressionException;
+import org.sergut.diceroller.montecarlo.DiceResult;
+import org.sergut.diceroller.montecarlo.MontecarloSimulator;
+
+public class Simulator {
+    private static void usage() {
+	System.out.println("USAGE: java Simulator <test-dice-expression> <operand> <goal-expression>");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	if (args.length != 3) {
+	    usage();
+	    return;
+	}
+	Simulator sim = new Simulator();
+	sim.simulateDice(args[0], args[1], args[2]);
+    }
+    
+    private void simulateDice(String testDice, String op, String goalDice) {
+	int maxRolls = 100000;
+	try {
+	    MontecarloSimulator simulator = MontecarloSimulator.getInstance();
+	    DiceResult result = simulator.simulateDice(testDice, op, goalDice, maxRolls);
+	    showResults(result.successRolls, result.totalRolls);
+	} catch (NumberFormatException ex) {
+	    System.out.println("There is an error with one of the numbers");
+	} catch (IllegalDiceExpressionException ex) {
+	    System.out.println("Invalid expression: " + ex.getExpression());
+	}
+    }
+
+    private void showResults(int successRolls, int totalRolls) {
+	double result = 100.0 * successRolls;
+	System.out.println(result / totalRolls + "%");
+    }
+
+}
