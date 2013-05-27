b722cb12c2fae99cd00389da1331fd43370c5beb
Sergio Gutierrez-Santos "Sergut <sergut@gmail>
2013-05-27 20:13:13 +0200

    Added a clone() method. Should be useful for testing purposes at least.

--- src/org/sergut/diceroller/savageworlds/SavageWorldsSimulationJob.java
+++ src/org/sergut/diceroller/savageworlds/SavageWorldsSimulationJob.java
@@ -19,5 +19,43 @@ public class SavageWorldsSimulationJob {
     public boolean includeWildAttack    = false;
     public boolean includeNonRapidAttack = true;
     public boolean includeRapidAttack    = false;
+    
+    public SavageWorldsSimulationJob() {
+	attackDice = null;
+	damageDice= null;
+	attackerWildCard = false;
+	defenderParry = -1;
+	defenderToughness = -1;
+	defenderShaken = false;
+	defenderWildCard = false;
+	includeBodyAttack = true;
+	includeArmAttack  = false;
+	includeHeadAttack = false;
+	includeNonWildAttack = true;
+	includeWildAttack    = false;
+	includeNonRapidAttack = true;
+	includeRapidAttack    = false;
+	maxIterations = 100000;
+    }
+
     // TODO: implement a fluent interface to build jobs
+    public SavageWorldsSimulationJob clone() {
+	SavageWorldsSimulationJob result = new SavageWorldsSimulationJob();
+	result.attackDice = this.attackDice;
+	result.damageDice = this.damageDice;
+	result.attackerWildCard = this.attackerWildCard;
+	result.defenderParry = this.defenderParry;
+	result.defenderToughness = this.defenderToughness;
+	result.defenderShaken = this.defenderShaken;
+	result.defenderWildCard = this.defenderWildCard;
+	result.includeBodyAttack = this.includeBodyAttack;
+	result.includeArmAttack  = this.includeArmAttack;
+	result.includeHeadAttack = this.includeHeadAttack;
+	result.includeNonWildAttack = this.includeNonWildAttack;
+	result.includeWildAttack    = this.includeWildAttack;
+	result.includeNonRapidAttack = this.includeNonRapidAttack;
+	result.includeRapidAttack    = this.includeRapidAttack;
+	result.maxIterations = this.maxIterations;
+	return result;
+    }
 }
