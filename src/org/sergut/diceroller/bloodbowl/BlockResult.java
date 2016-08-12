5afd06261ff3e82c4dc1022cdf3ec309703bb683
Sergio Gutierrez-Santos 'Sergut <sergut @ gmail>
2016-08-12 10:52:06 +0100

    Added a bit of documentation for this ternary type.

--- src/org/sergut/diceroller/bloodbowl/BlockResult.java
+++ src/org/sergut/diceroller/bloodbowl/BlockResult.java
@@ -3,10 +3,26 @@ package org.sergut.diceroller.bloodbowl;
 import java.util.Comparator;
 
 public enum BlockResult implements Comparable<BlockResult>{
-	// TURNOVER includes "Attacker Down" is as bad as "Both Down without Block", 
+	/* TURNOVER aims to include:
+	 * - "Attacker Down" 
+	 * - "Both Down", attacker without Block
+	 * It really does not matter whether the defender has Block or not, 
+	 * as it is a Turnover anyway, which is the worst result. 
+	 */ 
 	TURNOVER,
-	// NOBODY_DOWN includes "Push" and "Both Down when both have Block"
+	/* NOBODY_DOWN aims to include all those results that are 
+	 * neither Turnovers nor involve an Armor roll: 
+	 * - "Push"
+	 * - "Both Down" when both have Block
+	 * - "Both Down" when the defender has Wrestle (both players are 
+	 *   placed prone, but none is Knocked Down
+	 * - "Defender Stumbles" and has Dodge
+	 */
 	NOBODY_DOWN, 
-	// KNOCK_DOWN includes all results in which only the defender is Knocked Down
+	/* KNOCK_DOWN aims to includes all those results in which 
+	 * only the defender is Knocked Down: 
+	 * - "Defender Down"
+	 * - "Defender Stumbles" and cannot use Dodge
+	 */
 	KNOCK_DOWN;
 }
