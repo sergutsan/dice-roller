4a05088186ee3d12af709411236c66e2bf92340b
Sergio Gutierrez-Santos 'Sergut <sergut @ gmail>
2016-08-12 11:24:54 +0100

    Added some new polimorphic classes to calculate the ratio of casualties in different cases.

---
+++ src/org/sergut/diceroller/bloodbowl/casualty/CasualtySimulator.java
@@ -0,0 +1,6 @@
+package org.sergut.diceroller.bloodbowl.casualty;
+
+public interface CasualtySimulator {
+
+	int getCasualtyRatio(int armorToBeat);
+}
---
+++ src/org/sergut/diceroller/bloodbowl/casualty/MightyBlowCasualtySimulator.java
@@ -0,0 +1,11 @@
+package org.sergut.diceroller.bloodbowl.casualty;
+
+public class MightyBlowCasualtySimulator implements CasualtySimulator {
+
+	@Override
+	public int getCasualtyRatio(int armorToBeat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
